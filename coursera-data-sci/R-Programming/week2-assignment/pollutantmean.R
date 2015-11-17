cf65936cb0faed9df11a69eed51a392b7203a144
nkodam <nkkodam@gmail.com>
2015-11-17 02:44:13 -0600

    home commit 11/17.

--- coursera-data-sci/R-Programming/week2-assignment/pollutantmean.R
+++ coursera-data-sci/R-Programming/week2-assignment/pollutantmean.R
@@ -1,8 +1,10 @@
 # Part 1
 #Write a function named 'pollutantmean' that calculates the mean of a pollutant (sulfate or nitrate) across a specified list of monitors. 
 #The function 'pollutantmean' takes three arguments: 'directory', 'pollutant', and 'id'. 
-#Given a vector monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter data from the directory specified in the 'directory' argument 
-# and returns the mean of the pollutant across all of the monitors, ignoring any missing values coded as NA. 
+#Given a vector monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter data 
+#from the directory specified in the 'directory' argument 
+# and returns the mean of the pollutant across all of the monitors, 
+#ignoring any missing values coded as NA. 
 # #A prototype of the function is as follows
 
 
@@ -24,4 +26,22 @@
 
 #######################################################################
 
+
+pullutantmean <- function(directory, pollutant, id = 1:332){
+        
+        pollutdata <- numeric()
+        
+        for(i in seq_along(id)){
+                
+                # create filepath
+                monitorname <- formatC(i, flag = 0, width = 3)
+                filepath <- paste0(directory, "/", monitorname, ".csv")
+                
+                # load into dataframe object
+                dframe <- read.csv(filepath)
+                
+                pollutdata <- c(pollutdata, dframe[[pollutant]])
+        }
+        mean(df$pollutant, na.rm = TRUE)
+}
 
