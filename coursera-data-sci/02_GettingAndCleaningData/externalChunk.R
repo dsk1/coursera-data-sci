23a4ae36c8657fe0cec169d67aa7e91a52e14a4d
dsk1 <nkkodam@gmail.com>
2015-12-03 00:27:15 -0600

    home commit 12/02

---
+++ coursera-data-sci/02_GettingAndCleaningData/externalChunk.R
@@ -0,0 +1,13 @@
+# make this an external chunk that can be included in any file
+options(width = 100)
+opts_chunk$set(message = F, error = F, warning = F, comment = NA, fig.align = 'center', dpi = 100, tidy = F, cache.path = '.cache/', fig.path = 'fig/', cache=TRUE)
+
+options(xtable.type = 'html')
+knit_hooks$set(inline = function(x) {
+  if(is.numeric(x)) {
+    round(x, getOption('digits'))
+  } else {
+    paste(as.character(x), collapse = ', ')
+  }
+})
+knit_hooks$set(plot = knitr:::hook_plot_html)
