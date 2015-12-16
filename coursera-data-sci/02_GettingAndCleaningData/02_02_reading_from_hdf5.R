5816a48b754ecc481cf52ddda1ee6449d783cb29
dsk1 <nkkodam@gmail.com>
2015-12-15 19:33:03 -0600

    home commit

---
+++ coursera-data-sci/02_GettingAndCleaningData/02_02_reading_from_hdf5.R
@@ -0,0 +1,13 @@
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+
+library(rhdf5)
+created = h5createFile("example.h5")
+created
+
+created = h5createGroup("example.h5", "foo")
+created = h5createGroup("example.h5", "baa")
+created = h5createGroup("example.h5", "foo/foobaa")
+h5ls("example.h5")
+
+# write a group
