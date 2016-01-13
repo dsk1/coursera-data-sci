d413d815aefc023e290a4f8f4ec9c98a0520230a
Naveen Kodam <nkodam@assisted.com>
2016-01-13 13:46:30 -0600

    work commit. 1/13. nk.

---
+++ coursera-data-sci/04_Swirl/04_01_PrinciplesOfAnalyticalGraphs.R
@@ -0,0 +1,13 @@
+# install swirl package
+
+install.packages("swirl")
+packageVersion("swirl")
+library("swirl")
+
+install_from_swirl("Exploratory Data Analysis")
+
+# start swirl
+swirl()
+info()
+
+getwd()
