e9a12669f2fbb65d27ba840f26c5a878521f6644
nkodam <nkkodam@gmail.com>
2016-01-16 19:56:20 -0600

    home commit. 1/16.

---
+++ coursera-data-sci/04_ExploratoryDataAnalysis/04_01_Swirl.R
@@ -0,0 +1,10 @@
+# install swirl
+install.packages("swirl")
+packageVersion("swirl")
+library(swirl)
+
+# install exploratory data analysis from swirl
+install_from_swirl("Exploratory Data Analysis")
+
+# start swirl
+swirl()
