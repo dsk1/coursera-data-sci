255b81651186b067199ea5e77bf0c090b2b4fa92
Naveen Kodam <nkodam@assisted.com>
2016-02-25 16:43:13 -0600

    work commit.

--- 03_ExploratoryDataAnalysis/03_Air_Pollution_Case_Study.R
+++ 03_ExploratoryDataAnalysis/03_Air_Pollution_Case_Study.R
@@ -18,7 +18,7 @@ download.file(file2Url, destfile = "./data/RD_501_88101_2012-0.txt")
 list.files("./data")
 
 #####################
-
+# read the first file (exclude header line)
 pm0 <- read.table("./data/RD_501_88101_1999-0.txt", comment.char = "#", header = FALSE, sep = "|", na.strings = "")
 dim(pm0) 
 head(pm0)
@@ -46,8 +46,9 @@ class(x0)
 summary(x0)
 
 # see the proportion of missing values. about 11% are missing.
-mean(is.na(x0))
+mean(is.na(x0)) # 11% NAs
 
+# read the second file (exclude the header line)
 pm1 <- read.table("./data/RD_501_88101_2012-0.txt", comment.char = "#", header = FALSE, sep = "|", na.strings = "")
 head(pm1)
 dim(pm1)
@@ -57,8 +58,9 @@ cnames <- readLines("./data/RD_501_88101_2012-0.txt", 1)
 cnames
 strsplit(cnames, "|", fixed = TRUE)
 strsplit(cnames, "|", fixed = TRUE)[[1]]
-cnames
+
 cnames <- strsplit(cnames, "|", fixed = TRUE)[[1]]
+# replace the spaces with dots
 cnames <- make.names(cnames)
 cnames
 
@@ -74,8 +76,8 @@ summary(x0)
 summary(x1) # why is there -ve data?
 
 # about 5% are missing
-mean(is.na(x0))
-mean(is.na(x1))
+mean(is.na(x0)) #11%
+mean(is.na(x1)) #5.5%
 
 # see the sample.values in relation to each other on box plot
 boxplot(x0, x1)
@@ -84,15 +86,39 @@ boxplot(log10(x0), log10(x1))
 
 # pluck out the negative values and see what we can tell
 negative1 <- x1 < 0
-sum(negative1, na.rm = TRUE)
-sum(x1) # 26K
-mean(negative1, na.rm = TRUE) # only 2%
+# How many are negative?
+sum(negative1, na.rm = TRUE) #4.9 K
 
-# lets take a look at the other values of negative values
+# how many total?
+length(x1) #169k
+
+# how many NAs
+sum(is.na(x1)) #9.3k
+
+# how many -ves
+length(x1[x1<0 & !is.na(x1)]) #4.9 k
+sum(x1<0, na.rm=TRUE) # 4.9k 
+
+# remaining: non NAs and postivies
+sum(x1>=0, na.rm = TRUE) #155.2K
+
+summary(x1)
+#% of NAs 
+mean(is.na(x1))  # 5.5%
+
+# % of -ves
+mean(x1<0, na.rm = TRUE) # 3.1%
+
+
+# lets take a look at the other variables of Negative or NA values
 # for example when are these -ve values occuring?
 # lets look at the date variable.
 # 
+# create a logical vector of negatives
 
+negatives1 <- x1<0
+
+# format the Date variable
 str(pm1$Date)
 dates1 <- pm1$Date
 dates1 <- as.Date(as.character(dates1), "%Y%m%d" )
@@ -103,35 +129,55 @@ hist(dates1, "month")
 
 # take a look at where the -ve values are occuring 
 hist(dates1[negative1], "month") # lot of -ves in dec, jan, feb timeframe
-
 # investigate the -ve values later as it is only 2% of the total data
+
 
 ##### analyze the pollution at one location #####
 # pick out on monitor thats there in 1999 and 2012 and compare the two
 # pick out the distinct counties and sites in New York state
 
+# view the subset data for NY state
+subset(pm0, State.Code ==36)
+subset(pm1, State.Code ==36)
+
+# create a vectore of the subset data
 site0 <- unique(subset(pm0, State.Code == 36, c(County.Code, Site.ID)) )
 site1 <- unique(subset(pm1, State.Code == 36, c(County.Code, Site.ID)) )
 
+# paste (concatinate) state and county to form a vector of unique keys
 site0 <- paste(site0[,1], site0[,2], sep = ".")
 site1 <- paste(site1[,1], site1[,2], sep = ".")
 
-head(site0)
-head(site1)
+length(site0) # 33 monitors
+length(site1) # 18 monitors
 
-str(site0) # 33 monitors
-str(site1) # 18 monitors
-
+# intersect to get the sites monitored in both years
 intersect(site0, site1)
+bothyrmtrs <- intersect(site0, site1)
 
-bothyrmonitor <- intersect(site0, site1)
+bothyrmtrs
 
-bothyrmonitor
-
+# Now compare the 2 years data for these monitors
 # pick out one monitor/site's data from both years in NY
+# 
 # first create the county.site column in pm data
+# pm0$county.site <- paste(pm0$County.Code, pm0$Site.ID, sep = ".")
+?with
 pm0$county.site <- with(pm0, paste(County.Code, Site.ID, sep = "."))
 pm1$county.site <- with(pm1, paste(County.Code, Site.ID, sep = "."))
 
-# pickout the data that is for the monitory in bothyrsmonitor
+
+# pickout the data that is for the monitor in bothyrsmonitor
+pm0sub <- subset(pm0, county.site %in% bothyrmtrs)
+pm1sub <- subset(pm1, county.site %in% bothyrmtrs)
+# check the subset data
+str(pm0sub)
+dim(pm0sub)
+
+# compare the county.site values to the bothyrmtrs vector
+sort(unique(pm0sub$county.site))
+sort(bothyrmtrs)
+
+
+
 
