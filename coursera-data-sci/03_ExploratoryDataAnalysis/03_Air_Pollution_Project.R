d0eac7c8bc21f64a011f2c7fd85090ffce4e808d
nkodam <nkkodam@gmail.com>
2016-02-29 22:36:34 -0600

    commit.

--- coursera-data-sci/03_ExploratoryDataAnalysis/03_Air_Pollution_Project.R
+++ coursera-data-sci/03_ExploratoryDataAnalysis/03_Air_Pollution_Project.R
@@ -1,8 +1,20 @@
 getwd()
 setwd("C:/RWDir/coursera-data-sci/coursera-data-sci/03_ExploratoryDataAnalysis/")
-fileURL <- "http://d396qusza40orc.cloudfront.net/exdata-data-FNEI_data.zip"
-if(!file.exists("./data")){
-  dir.create("data")
-}
 
-download.file(fileURL, destfile = "./data/exdata-data-FNEI_data.zip")
+# read the files
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+str(NEI)
+head(NEI)
+str(SCC)
+head(SCC)
+install.packages("plyr")
+library(plyr)
+
+yrtotals <- ddply(NEI, "year", summarise,
+                  total = sum(Emissions))
+head(yrtotals)
+yrtotals
+plot(yrtotals$year, yrtotals$total)
+
